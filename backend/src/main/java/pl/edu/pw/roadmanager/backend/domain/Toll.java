c7e47c9d037f91e470ccbf370714f06382bd92f6
Hubert Piłka <hubert.pilka@outlook.com>
2022-10-28 20:32:45 +0200

    add Toll, User, Vehicle domain models

--- backend/src/main/java/pl/edu/pw/roadmanager/backend/domain/Toll.java
+++ backend/src/main/java/pl/edu/pw/roadmanager/backend/domain/Toll.java
@@ -1,2 +1,32 @@
-package pl.edu.pw.roadmanager.backend.domain;public class Toll {
+package pl.edu.pw.roadmanager.backend.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "toll")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Toll {
+    @Id
+    @SequenceGenerator(name = "toll_sequence", sequenceName = "toll_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "toll_sequence")
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "name", updatable = true)
+    private String name;
+
+    @Column(name = "price_per_kilometer", updatable = true)
+    private float pricePerKilometer;
 }
--- backend/src/main/java/pl/edu/pw/roadmanager/backend/domain/User.java
+++ backend/src/main/java/pl/edu/pw/roadmanager/backend/domain/User.java
@@ -1,2 +1,38 @@
-package pl.edu.pw.roadmanager.backend.domain;public class User {
+package pl.edu.pw.roadmanager.backend.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @SequenceGenerator(name = "user_sequence", sequenceName = "user_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_sequence")
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "email", updatable = true)
+    private String email;
+
+    @Column(name = "first_name", updatable = true)
+    private String firstName;
+
+    @Column(name = "last_name", updatable = true)
+    private String lastName;
+
+    @Column(name = "phone_number", updatable = true)
+    private String phoneNumber;
 }
