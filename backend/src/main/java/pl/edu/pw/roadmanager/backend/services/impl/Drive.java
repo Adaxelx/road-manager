06a5c8db4fb45bc49efe99740a2ac2bacb42199c
Peszko-Patryk <patrickpeszko@gmail.com>
2022-11-10 18:40:28 +0100

    added service annotation

--- backend/src/main/java/pl/edu/pw/roadmanager/backend/services/impl/Drive.java
+++ backend/src/main/java/pl/edu/pw/roadmanager/backend/services/impl/Drive.java
@@ -2,12 +2,14 @@ package pl.edu.pw.roadmanager.backend.services.impl;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import pl.edu.pw.roadmanager.backend.domain.Vehicle;
 import pl.edu.pw.roadmanager.backend.dto.VehicleDTO;
 import pl.edu.pw.roadmanager.backend.repositories.VehicleRepository;
 import pl.edu.pw.roadmanager.backend.services.CepikAPI;
 import pl.edu.pw.roadmanager.backend.services.DriveAPI;
 
+@Service
 public class Drive implements DriveAPI {
 
     @Autowired
