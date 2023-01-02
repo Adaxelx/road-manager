0cb314c66adefdb3907af323543098b209757c6d
Prz-mek <kwasniewski.pj@gmail.com>
2023-01-02 20:46:20 +0100

    database script

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO APP_USER VALUES (1, 'jkowalski@example.com', 'Jan', 'Kowalski','123456789');
+INSERT INTO APP_USER VALUES (2, 'anowak@example.com', 'Anna', 'Nowak','123123123');
+
+INSERT INTO SUBSCRIPTION_TYPE VALUES (1, 'Bilet roczny dla samochodów osobowych', 365, 259.99, 'CAR');
+INSERT INTO SUBSCRIPTION_TYPE VALUES (2, 'Bilet roczny dla ciężarówek', 365, 469.99, 'TRUCK');
+INSERT INTO SUBSCRIPTION_TYPE VALUES (3, 'Miesieczny dla samochodów osobowych', 30, 49.99, 'CAR');
+INSERT INTO SUBSCRIPTION_TYPE VALUES (4, 'Miesieczny dla ciężarówek', 30, 69.99, 'TRUCK');
