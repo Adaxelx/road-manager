5ca9dcee2c620b869f44aee0cb0565bb3e5245de
Peszko-Patryk <patrickpeszko@gmail.com>
2023-01-02 21:56:03 +0100

    Added PayUAPI with implementation.

---
+++ backend/src/main/java/pl/edu/pw/roadmanager/backend/services/PayUAPI.java
@@ -0,0 +1,6 @@
+package pl.edu.pw.roadmanager.backend.services;
+
+public interface PayUAPI {
+
+    boolean makePayment(int blik);
+}
---
+++ backend/src/main/java/pl/edu/pw/roadmanager/backend/services/impl/PayU.java
@@ -0,0 +1,11 @@
+package pl.edu.pw.roadmanager.backend.services.impl;
+
+import pl.edu.pw.roadmanager.backend.services.PayUAPI;
+
+public class PayU implements PayUAPI {
+
+    @Override
+    public boolean makePayment(int blik) {
+        return true;
+    }
+}
